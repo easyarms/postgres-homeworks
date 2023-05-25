1c59dfb27d2925ef0757b6af984aa65a3dc41499
d.larin <larin.dn@icloud.com>
2023-05-25 23:14:53 +0400

    homework_3.1

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,7 +1,7 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-SELECT customers.company_name, employees.first_name || ' ' || employees.last_name as employee_name
+SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) as employee_name
 FROM customers
 JOIN orders ON orders.customer_id = customers.customer_id
 JOIN employees ON employees.employee_id = orders.employee_id
@@ -21,17 +21,15 @@ WHERE products.discontinued = 0 AND products.units_in_stock < 25 AND (categories
 ORDER BY products.units_in_stock ASC
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-SELECT company_name
+SELECT distinct company_name
 FROM customers
-WHERE customer_id
-NOT IN (
-	SELECT customer_id
-	FROM orders)
+LEFT JOIN orders USING(customer_id)
+WHERE order_id IS NULL
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
 SELECT DISTINCT product_name
 FROM products
-WHERE product_id IN (
-	SELECT product_id FROM order_details
-	WHERE quantity = 10)
+WHERE product_id = ANY
+(SELECT product_id FROM order_details
+WHERE quantity = 10)
